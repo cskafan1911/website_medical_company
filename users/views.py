2e42f4db4d9513c27160384a82e7f80f75185c6c
Dmitry Dronov <cskafan1911@gmail.com>
2024-04-22 19:38:20 +0300

    Права доступа для редактирования врачей.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
+from django.http import Http404
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.encoding import force_bytes
@@ -185,36 +187,39 @@ class DoctorDetailView(DetailView):
     model = Doctor
 
 
-class DoctorUpdateView(UpdateView):
+class DoctorUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     """
     Класс для редактирования объекта врач.
     """
 
     model = Doctor
     form_class = DoctorForm
+    permission_required = 'users.change_doctor'
     success_url = reverse_lazy('users:doctor_list')
     extra_context = {
         'title': 'Заполните форму для редактирования информации о враче'
     }
 
 
-class DoctorCreateView(CreateView):
+class DoctorCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     """
     Класс для добавления зарегистрированного пользователя в объект врач.
     """
 
     model = Doctor
     form_class = DoctorForm
+    permission_required = 'users.add_doctor'
     success_url = reverse_lazy('users:doctor_list')
     extra_context = {
         'title': 'Заполните форму для добавления врача'
     }
 
 
-class DoctorDeleteView(DeleteView):
+class DoctorDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     """
     Класс для удаления объекта врач.
     """
 
     model = Doctor
+    permission_required = 'users.delete_doctor'
     success_url = reverse_lazy('users:doctor_list')
